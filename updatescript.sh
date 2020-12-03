26a05e73fc3f22b1098a199596074a7ed2ec2198
SHANMUKH-CH <shanmukhrodofatos@gmail.com>
2020-12-03 17:07:43 +0530

    updating

--- updatescript.sh
+++ updatescript.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get autoremove && sudo apt full-upgrade -y
+sudo apt-get update && sudo apt-get -y upgrade && sudo apt-get -y dist-upgrade && sudo apt-get autoremovey
